45ad0fbbf242b1576d3137aa5089e44f6d50fee0
Chirayu Giri <90922566+Chirayugiri@users.noreply.github.com>
2026-01-02 10:20:25 +0530

    updated next config file for deployment on aws

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "ik.imagekit.io",
-  //       port: "",
-  //     },
-  //   ],
-  // },
+  output: "standalone",
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     domains: ["www.flaticon.com"],
   },
